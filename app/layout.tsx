ff190145267b6cbb654d4fb9ca2dfda5e8c6aff7
kn <nguyentuanpkn@gmail.com>
2023-11-14 08:20:51 +0700

    head

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "Ndknitor",
   description: '',
+  manifest : "/site.webmanifest",
+  icons: ["/favicon-32x32.png", "/favicon-16x16.png"]
 }
 
 
